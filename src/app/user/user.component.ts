0268406d53c9636e647d0871d6bd0424c314a1ae
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2025-03-04 12:58:23 +0530

    Added Signal Comments

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS }  from '../dummy-users';
 import { signal } from '@angular/core';
+/*
+Angular Signals provide a reactive, efficient, and predictable way to manage state and UI updates. 
+They simplify change detection and improve performance, making them a great alternative to traditional state management approaches.
 
+A Signal in Angular is a reactive state container that automatically tracks dependencies and updates when its value changes.
+It provides a simpler, more efficient alternative to BehaviorSubject, EventEmitter, or ChangeDetectorRef for managing state.
+*/
 const count = signal(0);
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
